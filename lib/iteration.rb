4c0e7577af7f4ec297f1aeae7443f0de57376aa9
Michael Calkins <m.calkins576@gmail.com>
2019-10-18 17:13:24 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -48,8 +48,7 @@ def total_even_pairs(src)
   row_index = 0
   while row_index < src.count do
     if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0 
-      new_array << src[row_index][0]
-      new_array << src[row_index][1]
+      new_array << src[row_index]
       end
     row_index += 1
   end
